7556f8973f208f0e7b6f990a1476a414bf910c5b
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-09-19 17:31:34 -0400

    clean the raw data and save the cleaned data to analysis_data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw data from open data toronto
+# Author: Talia Fabregas
+# Date: September 19 2024
+# Contact: talia.fabregas@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run 01-download_data.R first
+# Any other information needed? No
 
 #### Workspace setup ####
 library(tidyverse)
