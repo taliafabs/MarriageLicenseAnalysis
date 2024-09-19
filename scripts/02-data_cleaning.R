3fe01dec207d0af300b81ba4ac7b223e5d036739
talia fabregas <talia.fabregas@mail.utoronto.ca>
2024-09-19 17:10:19 -0400

    clean the data and save the cleaned data as a csv

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -11,34 +11,21 @@
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/raw_data/raw_data.csv")
 
-cleaned_data <-
+clean_data <-
   raw_data |>
   janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  separate(col = time_period,
+           into = c("year", "month"),
+           sep = "-") |>
+  mutate(date = lubridate::ymd(paste(year, month, "01", sep = "-"))
+         )
+
+# make a graph
+# marriage_license_trends <- clean_data |>
+#                             ggplot(aes(x = date, y = marriage_licenses)) +
+#                             geom_point()
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(clean_data, "outputs/data/analysis_data.csv")
